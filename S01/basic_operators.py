2c6283b1f402da851298995b81e758d79c9b4e45
iamvee <naeini.v@gmail.com>
2019-04-13 08:37:35 -0700

    trying print multiple vars in one line

---
+++ S01/basic_operators.py
@@ -0,0 +1,19 @@
+num_1 = 10.1111
+num_2 = 100
+
+num_3 = num_1 * num_2
+num_4 = num_2 - num_1
+
+print(num_3, num_4)
+
+
+# string operators
+word_one = "hello "
+word_two = "world."
+
+text = word_one + word_two
+print(text)
+
+text = "All work and no play makes Jack a dull boy!"
+paragraph = text * 10
+print(paragraph)
--- S01/variables_in_python.py
+++ S01/variables_in_python.py
@@ -2,6 +2,4 @@ number = 100
 real_number = 1.234
 text = "Hello World"
 
-print(number)
-print(real_number)
-print(text)
+print(number, real_number, text)
