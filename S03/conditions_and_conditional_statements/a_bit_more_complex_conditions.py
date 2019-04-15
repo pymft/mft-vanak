4dfbb5b83cdf32f0c73d335bb6dad16968f092da
iamvee <naeini.v@gmail.com>
2019-04-15 06:41:05 -0700

    write prev. conditional statement in one line

--- S03/conditions_and_conditional_statements/a_bit_more_complex_conditions.py
+++ S03/conditions_and_conditional_statements/a_bit_more_complex_conditions.py
@@ -3,11 +3,12 @@
 lower_limit = 10
 upper_limit = 20
 
-num = 35
+num = 15
+#
+# cond_1 = num > lower_limit
+# cond_2 = num < upper_limit
+#
+# cond = cond_1 and cond_2
 
-cond_1 = num > lower_limit
-cond_2 = num < upper_limit
-
-cond = cond_1 and cond_2
-
+cond = (num > lower_limit) and (num < upper_limit)
 print(cond)
