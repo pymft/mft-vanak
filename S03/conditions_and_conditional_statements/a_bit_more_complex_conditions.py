70ee7858c546b7692eaa20b3b1e0c2710ed4615d
iamvee <naeini.v@gmail.com>
2019-04-15 06:36:21 -0700

    getting started w/ conditional statements

---
+++ S03/conditions_and_conditional_statements/a_bit_more_complex_conditions.py
@@ -0,0 +1,13 @@
+
+
+lower_limit = 10
+upper_limit = 20
+
+num = 35
+
+cond_1 = num > lower_limit
+cond_2 = num < upper_limit
+
+cond = cond_1 and cond_2
+
+print(cond)
---
+++ S03/conditions_and_conditional_statements/test_bool_types.py
@@ -0,0 +1,19 @@
+# bool
+cond_one = True
+cond_two = False
+
+print(type(cond_one))
+
+# # int + int
+# value = 1000
+# neg_val = -value
+# num = 10 + 2
+
+result = 10 > 2
+print(result)
+
+number = 10
+# :=
+check_equality = 10 == number
+print(check_equality)
+
