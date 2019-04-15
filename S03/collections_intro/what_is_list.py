3b3ae9fa48eef748d35eefbffe8e442309841e12
iamvee <naeini.v@gmail.com>
2019-04-15 08:57:40 -0700

    an intro to lists

---
+++ S03/collections_intro/what_is_list.py
@@ -0,0 +1,8 @@
+my_list = [9, 1, 2, 3, 4, 5, 6]
+print(my_list)
+print(type(my_list))
+
+length_of_my_list = len(my_list)
+print("the length of `my_list` --> ", length_of_my_list)
+
+print("the first element of `my_list` --> ", my_list[0])
