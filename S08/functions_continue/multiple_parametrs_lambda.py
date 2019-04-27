e2dfd1dda5c7430c7ba9f2f5b864b2df6b07a4c8
iamvee <naeini.v@gmail.com>
2019-04-27 06:51:12 -0700

    rewrite lambda function w/ two input vars

--- S08/functions_continue/multiple_parametrs_lambda.py
+++ S08/functions_continue/multiple_parametrs_lambda.py
@@ -1,10 +1,10 @@
 distance = lambda p: (p[0] ** 2 + p[1] ** 2) ** 0.5
+distance_new = lambda x, y: (x ** 2 + y ** 2) ** 0.5
 
-point = (3, 4)
-res_1 = distance(point)
 
-new_point = (12, 5)
-res_2 = distance(new_point)
+res_1 = distance_new(3, 4)
+
+res_2 = distance_new(12, 5)
 
 print(res_1, res_2)
 
