0eaccf1fbd876ac7b0e0f0f94afe4cf92a699861
iamvee <naeini.v@gmail.com>
2019-05-04 09:01:39 -0700

    jesus christ! miller's super fast method

--- S11/check_code_runtime.py
+++ S11/check_code_runtime.py
@@ -16,7 +16,53 @@ def is_prime_new(n):
             return False
     return True
 
-prime = 1398269
+
+def is_prime_3(n):
+    """
+    Miller-Rabin primality test.
+
+    A return value of False means n is certainly not prime. A return value of
+    True means n is very likely a prime.
+    """
+    if n != int(n):
+        return False
+    n = int(n)
+    # Miller-Rabin test for prime
+    if n == 0 or n == 1 or n == 4 or n == 6 or n == 8 or n == 9:
+        return False
+
+    if n == 2 or n == 3 or n == 5 or n == 7:
+        return True
+    s = 0
+    d = n - 1
+    while d % 2 == 0:
+        d >>= 1
+        s += 1
+    assert (2 ** s * d == n - 1)
+
+    def trial_composite(a):
+        if pow(a, d, n) == 1:
+            return False
+        for i in range(s):
+            if pow(a, 2 ** i * d, n) == n - 1:
+                return False
+        return True
+
+    for i in range(8):  # number of trials
+        a = random.randrange(2, n)
+        if trial_composite(a):
+            return False
+
+    return True
+
+prime =  85053461164796801949539541639542805770666392330682673302530819774105141531698707146930307290253537320447270457
+
+# miller approach
+t1 = time.time()
+res = is_prime_3(prime)
+t2 = time.time()
+print("Elapsed Time : ", t2 - t1, "seconds", res)
+
 
 # new approach
 t1 = time.time()
